2ceb0bdbd38cacfd44bed0a36cd0f929f29643fa
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-28 13:36:30 +0100

    message using components html and ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  intervalSub;
-
-  ngOnInit() {
-    this.intervalSub = setInterval(() => {
-      console.log('Hello from ngOnInit');
-    }, 1000);
-  }
-
-  ngOnDestroy() {
-    if (this.intervalSub) {
-      clearInterval(this.intervalSub);
-    }
-  }
+export class AppComponent {
+  title = 'Hello World';
+  text = 'I am writing on behalf of Angular';
 }
