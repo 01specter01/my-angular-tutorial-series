c8b3f43c4f610f7fda778d788fa77a050adab236
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-28 15:46:42 +0100

    error to resolfe childMessage not showing in browser

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   @Input() childMessage: string;
-  
+
   constructor() {}
 
   ngOnInit(): void {}
